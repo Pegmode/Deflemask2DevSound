d437246efb8e085256b79caa652bdfb41ecb999c
Pegmode <dchu@ualberta.ca>
2020-02-18 18:20:48 -0700

    started cleaner OOP version

---
+++ deflemask2DevSoundN.py
@@ -0,0 +1,94 @@
+
+import zlib,sys,struct,os,argparse
+DEBUG = False
+PRINTSTAT = False
+import pdb
+
+class DmfGBModule():
+    def __init__(self,filepath):
+        self.songName = ""
+        self.authorName = ""
+        self.timeBase = None
+        self.tickTime1 = None
+        self.tickTime2 = None
+        self.engineRefreshStd = None
+        self.customRefreshFlag = None
+        self.rowsPerPattern = None
+        self.patterMatrixRowCount = None
+        self.effectColSize = 1
+
+        self.patternMatrix = []
+        self.instruments = []
+        self.waveTables = []
+
+        self.parseFromFile(filepath)
+
+    def parseFromFile(self,filepath):#
+        #file handling and decompression
+        decompressedDMF = self.__fileDecompress(filepath)
+        self.__headerCheck(decompressedDMF)
+
+        currentIndex = 18 #Block Lengths are variable so we need to keep track of current index
+        currentBlockLength = decompressedDMF[currentIndex]
+        #songName
+        self.songName = self.__parseString(decompressedDMF[currentIndex+1:currentIndex + currentBlockLength+1])
+        printSt("Song Name: {}".format(self.songName))
+        #authorName
+        currentIndex,currentBlockLength = self.__moveToNextBlock(currentIndex,currentBlockLength,decompressedDMF)
+        self.authorName = self.__parseString(decompressedDMF[currentIndex+1:currentIndex + currentBlockLength+1])
+        printSt("Author: {}".format(self.authorName))
+
+
+
+
+    def __moveToNextBlock(self,currentIndex,currentBlockLength,decompressedDMF):
+        currentIndex += currentBlockLength + 1
+        currentBlockLength = decompressedDMF[currentIndex]
+        return currentIndex,currentBlockLength
+
+    def __fileDecompress(self,filepath):#
+        f = open(filepath,'rb')
+        uncompressedDMF = f.read()
+        f.close()
+        decompressedDMF = zlib.decompress(uncompressedDMF)
+        if DEBUG:
+            with open("DEBUG.bin","wb") as f:
+                f.write(decompressedDMF)
+        return decompressedDMF
+
+    def __headerCheck(self,decompressedDMF):#only checks for GB
+        if decompressedDMF[0:16].decode("utf-8") != ".DelekDefleMask.":
+            sys.exit("Bad .dmf: Incorrect Header")
+        if decompressedDMF[16] != 24: #version must be 24 for now
+            sys.exit("Incompatible .dmf: Incorrect .dmf version\nmodule version {},".format(decompressedDMF[16]))
+        if decompressedDMF[17] != 0x4: #GB system code
+            sys.exit("Incompatible .dmf: Only Game Boy system is supported")
+        printSt("Header Check Passed!\n")
+
+    def __parseString(self,stringBlock):
+        dest = ''
+        for char in stringBlock:
+            dest += chr(char)
+        dest = dest.replace(' ', '_') #make strings usable in Devsound
+        return dest
+
+def printSt(prints):
+    if PRINTSTAT:
+        print(prints)
+
+
+def main():
+    global DEBUG,PRINTSTAT
+    parse = argparse.ArgumentParser(description = "Convert Deflemask .DMF to DevSound asm source")
+    parse.add_argument('filepath',help = "path to .dmf module")#reiquired
+    parse.add_argument('-d','-debug','-D',action = "store_true",help = "debug mode flag")
+    parse.add_argument('-p','-P','-print',action = "store_true",help = "Print states flag: enable to print all parse states")
+    args = parse.parse_args()
+
+    DEBUG = args.d
+    PRINTSTAT = args.p
+
+    dmfModule = DmfGBModule(args.filepath)
+
+
+main()
