8f03847064ce9aec5143665c062cf52828ab3c7d
Daniel Chu <dchu@ualberta.ca>
2022-10-12 18:40:16 -0600

    Update from external tool
    updated this based on an external tool I'm working with

--- deflemask2DevSoundN.py
+++ deflemask2DevSoundN.py
@@ -1,8 +1,102 @@
+from http.client import NOT_ACCEPTABLE
+import zlib, sys, os
 
-import zlib,sys,struct,os,argparse
+
 DEBUG = False
-PRINTSTAT = False
-import pdb
+PRINTSTAT = True
+
+class GBInstrument():
+    def __init__(self,name):
+        self.name = name
+        self.loop_pos = 0
+        self.arp_macro = []
+        self.arp_loop = None#255 or None = no loop
+        self.arp_isfixed = False
+        self.duty_macro = []
+        self.duty_loop = None
+        self.wave_macro = []
+        self.wave_loop = None
+        #gb envelope
+        self.env_vol = 0
+        self.env_dir = 0#0 down 1 up
+        self.env_len = 0
+        self.snd_len = 0#64 = infinite
+    def set_arp_loop(self, val):
+        if val >= 255:
+            self.arp_loop = None
+        else:
+            self.arp_loop = val
+    def set_duty_loop(self, val):
+        if val >= 255:
+            self.duty_loop = None
+        else:
+            self.duty_loop = val
+    def set_wave_loop(self, val):
+        if val >= 255:
+            self.wave_loop = None
+        else:
+            self.wave_loop = val
+
+class DMFPatternRow():
+    noteTable = {
+			1: 'C#',
+			2: 'D-',
+			3: 'D#',
+			4: 'E-',
+			5: 'F-',
+			6: 'F#',
+			7: 'G-',
+			8: 'G#',
+			9: 'A-',
+			10: 'A#',
+			11: 'B-',
+			12: 'C-',
+            100: 'OFF',
+            0: "-"
+    }
+    def __init__(self,row):
+        self.row = row
+        self.note = None
+        self.octave = None
+        self.vol = 0
+        self.instrument = 0
+        self.effects = []
+        #debug+extra info
+        self.channel = 0
+        self.pattern = 0
+
+    def getReadableNote(self):
+        if self.note in [100,0]:#off or empty
+            return "---"
+        else:
+            return f"{self.noteTable[self.note]}{self.octave}"
+
+    def isBlank(self):#check if the row is empty of useful content
+        if self.note != 0:#check note
+            return False
+        for effect in self.effects:#check effects
+            if -1 not in effect:
+                return False
+        return True
+
+    def printRow(self):
+        print(self.getRowString())
+
+    def getRowString(self):#get a string representation of the row
+        effects = " "
+        for effect in self.effects:
+            if -1 in effect:
+                effects += "-- "
+            else:
+                effects += f"{effect[0]},{effect[1]} "
+        vol = self.vol
+        if vol == -1:
+            vol = ""
+        return f"{self.row}  {self.getReadableNote()}|{vol}|{self.instrument}|{effects}"
+
+    def printRowExtra(self):
+        row = self.getRowString()
+        print(f"{row}    ch {self.channel}")
 
 class DmfGBModule():
     def __init__(self,filepath):
@@ -14,11 +108,14 @@ class DmfGBModule():
         self.engineRefreshStd = None
         self.rowsPerPattern = None
         self.patternMatrixSize = None
-        self.effectColSize = 1
+        self.effectColSize = []#effect cols for channels 1-4
 
         self.patternMatrix = []
         self.instruments = []
         self.waveTables = []
+
+
+        self.rawPatterns = [[],[],[],[]]#raw patterns in unsorted .dmf format access goes like: rawPattern[channel][patternMatrixRow][patternRow]    Note that pattern matrix is not followed eg: patterns are duplicated
 
         self.parseFromFile(filepath)
 
@@ -26,6 +123,10 @@ class DmfGBModule():
         #file handling and decompression
         decompressedDMF = self.__fileDecompress(filepath)
         self.__headerCheck(decompressedDMF)
+
+        f = open("dmfUncompressed.bin", "wb")
+        f.write(decompressedDMF)
+        f.close()
 
         currentIndex = 18 #Block Lengths are variable so we need to keep track of current index
         currentBlockLength = decompressedDMF[currentIndex]
@@ -35,6 +136,7 @@ class DmfGBModule():
         #authorName
         currentIndex,currentBlockLength = self.__moveToNextBlock(currentIndex,currentBlockLength,decompressedDMF)
         self.authorName = self.__parseString(decompressedDMF[currentIndex+1:currentIndex + currentBlockLength+1])
+        currentIndex,currentBlockLength = self.__moveToNextBlock(currentIndex,currentBlockLength,decompressedDMF)
         printSt("Author: {}".format(self.authorName))
         #pattern highlight (bpm calc later)
         currentIndex += 2 #skip pattern highlight
@@ -66,8 +168,108 @@ class DmfGBModule():
         currentIndex += 1
 
         ## pattern matrix block
-
-
+        for i in range(4):#channels
+            self.patternMatrix.append([])
+            for j in range(self.patternMatrixSize):#patternMatrixRows
+                self.patternMatrix[i].append(decompressedDMF[currentIndex])
+                currentIndex += 1
+        
+        ##instruments
+        instrumentCount = decompressedDMF[currentIndex]
+        currentIndex += 1
+        for iInstrument in range(instrumentCount):
+            currentBlockLength = decompressedDMF[currentIndex]#size of instrument name
+            instrumentName =  self.__parseString(decompressedDMF[currentIndex+1:currentIndex + currentBlockLength+1])
+            print(f"instrumentname { instrumentName}")
+            currentIndex,currentBlockLength = self.__moveToNextBlock(currentIndex,currentBlockLength,decompressedDMF)
+            instrument = GBInstrument(instrumentName)
+            #parse Arp macro
+            currentIndex += 1
+            blockSize = decompressedDMF[currentIndex]
+            currentIndex += 1
+            for j in range(blockSize):
+                macroVal = decompressedDMF[currentIndex:currentIndex+4]#4 bytes per val
+                instrument.arp_macro.append(int.from_bytes(macroVal, "little",  signed="True") - 12)#offset for arp macro is + 12
+                currentIndex += 4
+            if (blockSize > 0):
+                instrument.set_arp_loop(decompressedDMF[currentIndex])
+                currentIndex += 1
+            instrument.arp_isfixed = decompressedDMF[currentIndex] == 1
+            currentIndex += 1
+            #parse Duty macro
+            blockSize = decompressedDMF[currentIndex]
+            currentIndex += 1
+            for j in range(blockSize):
+                macroVal = decompressedDMF[currentIndex:currentIndex+4]#4 bytes per val
+                instrument.duty_macro.append(int.from_bytes(macroVal, "little"))
+                currentIndex += 4
+            if (blockSize > 0):
+                instrument.set_duty_loop(decompressedDMF[currentIndex])
+                currentIndex += 1
+            #parse wave macro
+            blockSize = decompressedDMF[currentIndex]
+            currentIndex += 1          
+            for j in range(blockSize):
+                macroVal = decompressedDMF[currentIndex:currentIndex+4]#4 bytes per val
+                instrument.wave_macro.append(int.from_bytes(macroVal, "little"))
+                currentIndex += 4
+            if (blockSize > 0):
+                instrument.set_wave_loop(decompressedDMF[currentIndex])
+                currentIndex += 1
+            #parse envelope
+            instrument.env_vol = decompressedDMF[currentIndex]
+            currentIndex += 1   
+            instrument.env_dir = decompressedDMF[currentIndex]
+            currentIndex += 1                   
+            instrument.env_len = decompressedDMF[currentIndex]
+            currentIndex += 1    
+            instrument.snd_len = decompressedDMF[currentIndex]
+            currentIndex += 1    
+        
+        #wavetables parse
+        blockSize = decompressedDMF[currentIndex]#num wavetables
+        currentIndex += 1   
+        for i in range(blockSize):
+            currentIndex += 4 # we skip wavesize beacuse we only support gb
+            wave = []
+            for j in range(32):#number of samples gb wavetable supports
+                waveSample = decompressedDMF[currentIndex:currentIndex+4]
+                currentIndex += 4
+                wave.append(int.from_bytes(waveSample, "little"))
+        #parse Patterns
+        for i in range(4):#number of channels gb has, i = current channel
+            self.effectColSize.append(decompressedDMF[currentIndex])
+            currentIndex += 1 
+            for p in range(self.patternMatrixSize):
+                pattern = []
+                for r in range(self.rowsPerPattern):#r = row number
+                    row = DMFPatternRow(r)
+                    note = decompressedDMF[currentIndex:currentIndex + 2]#notes are 2 bytes
+                    currentIndex += 2  
+                    row.note = int.from_bytes(note, "little")
+                    octave = decompressedDMF[currentIndex:currentIndex + 2]#octave are 2 bytes
+                    currentIndex += 2  
+                    row.octave = int.from_bytes(octave, "little") 
+                    vol = decompressedDMF[currentIndex:currentIndex + 2]#volume
+                    currentIndex += 2  
+                    row.vol = int.from_bytes(vol, "little",signed="True")            
+                    for e in range(self.effectColSize[i]):#effect
+                        effectCode = decompressedDMF[currentIndex:currentIndex + 2]#effect code
+                        currentIndex += 2 
+                        effectVal = decompressedDMF[currentIndex:currentIndex + 2]#effect value
+                        currentIndex += 2   
+                        effect = (int.from_bytes(effectCode, "little",signed="True"), int.from_bytes(effectVal, "little",signed="True"))
+                        row.effects.append(effect)
+                    ins = decompressedDMF[currentIndex:currentIndex + 2]#instrument
+                    currentIndex += 2  
+                    row.ins = int.from_bytes(ins, "little") 
+                    #extra+debug row info
+                    row.channel = i
+                    if not row.isBlank():
+                        row.printRowExtra()
+                    pattern.append(row)
+            self.rawPatterns[i].append(pattern)
+                
     def __moveToNextBlock(self,currentIndex,currentBlockLength,decompressedDMF):
         currentIndex += currentBlockLength + 1
         currentBlockLength = decompressedDMF[currentIndex]
@@ -99,23 +301,14 @@ class DmfGBModule():
         dest = dest.replace(' ', '_') #make strings usable in Devsound
         return dest
 
+        
+
+
+
+
 def printSt(prints):
     if PRINTSTAT:
         print(prints)
 
 
-def main():
-    global DEBUG,PRINTSTAT
-    parse = argparse.ArgumentParser(description = "Convert Deflemask .DMF to DevSound asm source")
-    parse.add_argument('filepath',help = "path to .dmf module")#reiquired
-    parse.add_argument('-d','-debug','-D',action = "store_true",help = "debug mode flag")
-    parse.add_argument('-p','-P','-print',action = "store_true",help = "Print states flag: enable to print all parse states")
-    args = parse.parse_args()
-
-    DEBUG = args.d
-    PRINTSTAT = args.p
-
-    dmfModule = DmfGBModule(args.filepath)
-
-
-main()
+dmfModule = DmfGBModule("test.dmf")
